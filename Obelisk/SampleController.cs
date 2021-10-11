5b9ab639eb2aa8ef5c879a399742d2c18961c314
ArtificialBadger <isaaczais@hotmail.com>
2021-10-11 16:38:20 -0700

    Add Obelisk, an empty web app for testing ASP.NET integration

---
+++ Obelisk/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Obelisk
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+
+        [HttpGet("")]
+        public ActionResult<string> TestEndpoint()
+        {
+            return Ok("Test Successful");
+        }
+    }
+}
---
+++ Obelisk/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleInjector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Container = SimpleInjector.Container;
+
+namespace Obelisk
+{
+    public class Startup
+    {
+        private readonly Container container = new();
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new ProducesAttribute("application/json"));
+            });
+
+            services.AddSimpleInjector(container, options =>
+            {
+                // Allows service components to be injected into Api Controllers
+                options.AddAspNetCore()
+                        .AddControllerActivation();
+            });
+
+            services.AddMvcCore();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            container.Verify();
+        }
+    }
+}
