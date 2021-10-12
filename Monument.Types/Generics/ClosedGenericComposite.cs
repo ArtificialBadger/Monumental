7d892002e76cf6e6f32b4c92cc1991143904b348
ArtificialBadger <isaaczais@hotmail.com>
2021-10-12 16:30:10 -0700

    Commented this bad boi out to fix the SimpleInjector scenario

--- Monument.Types/Generics/ClosedGenericComposite.cs
+++ Monument.Types/Generics/ClosedGenericComposite.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Monument.Types
-{
-    public class ClosedGenericComposite : IGenericInterface<SimpleImplementation1>
-    {
-        public IEnumerable<IGenericInterface<SimpleImplementation1>> Inner { get; }
-        public ClosedGenericComposite(IEnumerable<IGenericInterface<SimpleImplementation1>> d) { Inner = d; }
-    }
-}
+//namespace Monument.Types
+//{
+//    public class ClosedGenericComposite : IGenericInterface<SimpleImplementation1>
+//    {
+//        public IEnumerable<IGenericInterface<SimpleImplementation1>> Inner { get; }
+//        public ClosedGenericComposite(IEnumerable<IGenericInterface<SimpleImplementation1>> d) { Inner = d; }
+//    }
+//}
