de2bdc345b0990ad8ed2b806d894b02e5f4f6c0b
iszais@microsoft.com <iszais@microsoft.com>
2022-04-27 10:16:25 -0700

    Added custom exceptions

---
+++ Monument.Types/Exceptions/IgnoredCustomException.cs
@@ -0,0 +1,25 @@
+﻿using Monument.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monument.Types.Exceptions
+{
+    [Ignore]
+    public class IgnoredCustomException : Exception
+    {
+        public IgnoredCustomException()
+        {
+        }
+
+        public IgnoredCustomException(string message)
+            : base(message)
+        {
+        }
+
+        public IgnoredCustomException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Monument.Types/Exceptions/SecondCustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monument.Types.Exceptions
+{
+    public class SecondCustomException : Exception
+    {
+        public SecondCustomException()
+        {
+        }
+
+        public SecondCustomException(string message)
+            : base(message)
+        {
+        }
+
+        public SecondCustomException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
